98ea34246b1bd99234d5b76bab49e2e471ea3b3e
sallysanban <johanna.eikou@gmail.com>
2022-11-14 15:52:23 +0800

    feat: added reading of file by line and cleaning up the whitespaces of line

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -2,16 +2,25 @@ import re
 
 filename = "Project 3/samplecode.txt"
 
-# def readFile(filename):
-#     file = open(filename)
-#     tokens = []
+def readFile(filename):
+    file = open(filename)
+    tokens = []
 
-#     for line in file.readlines():
-#         words = line.split()
-#         for i in words:
-#             tokens.append(i)
+    #reads the file and places each line in a list
+    for line in file.readlines():
+        words = line.split("\n")
+        for i in words:
+            tokens.append(i)
 
-#     return tokens
+    #gets rid of leading whitespaces in each line
+    for i in range(0, len(tokens)):
+        tokens[i] = tokens[i].strip()
+
+    for i in tokens:
+        if(i == ""):
+            tokens.remove(i)
+        
+    return tokens
 
 #MAIN CODE
-#print(readFile(filename))
+print(readFile(filename))
