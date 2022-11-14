ee2ed39c9e669a30b2e54fb24a65d01205178a07
sallysanban <johanna.eikou@gmail.com>
2022-11-14 15:41:03 +0800

    test: testing github on branch again

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -2,16 +2,16 @@ import re
 
 filename = "Project 3/samplecode.txt"
 
-def readFile(filename):
-    file = open(filename)
-    tokens = []
+# def readFile(filename):
+#     file = open(filename)
+#     tokens = []
 
-    for line in file.readlines():
-        words = line.split()
-        for i in words:
-            tokens.append(i)
+#     for line in file.readlines():
+#         words = line.split()
+#         for i in words:
+#             tokens.append(i)
 
-    return tokens
+#     return tokens
 
 #MAIN CODE
-print(readFile(filename))
+#print(readFile(filename))
