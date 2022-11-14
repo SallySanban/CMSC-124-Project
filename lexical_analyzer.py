22f20da4a68fa80244cadcaefcbe5b50849167a6
sallysanban <johanna.eikou@gmail.com>
2022-11-14 17:30:53 +0800

    feat: added findLexemes function, catches NUMBR literals

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -2,25 +2,53 @@ import re
 
 filename = "Project 3/samplecode.txt"
 
+#reads file and cleans each line in the file
 def readFile(filename):
     file = open(filename)
-    tokens = []
+    lines = []
 
     #reads the file and places each line in a list
     for line in file.readlines():
         words = line.split("\n")
         for i in words:
-            tokens.append(i)
+            lines.append(i)
 
     #gets rid of leading whitespaces in each line
-    for i in range(0, len(tokens)):
-        tokens[i] = tokens[i].strip()
+    for i in range(0, len(lines)):
+        lines[i] = lines[i].strip()
 
-    for i in tokens:
+    for i in lines:
         if(i == ""):
-            tokens.remove(i)
+            lines.remove(i)
         
-    return tokens
+    return lines
 
+#finds lexemes in code and groups them 
+def findLexemes(lines):
+    for i in lines:
+        #catches NUMBR literal
+        numbrLiteral = re.findall("[-]?\d+", i)
+        if(numbrLiteral):
+            for j in numbrLiteral:
+                symbolTable['NUMBR literal'].append(j)
+
+        #add other cases here
+    
 #MAIN CODE
-print(readFile(filename))
+symbolTable = {"function identifier": [],
+                "variable identifier": [],
+                "loop identifier": [],
+                "NUMBR literal": [],
+                "NUMBAR literal": [],
+                "YARN literal": [],
+                "TROOF literal": [],
+                "TYPE literal": [],
+                "keyword": [] #i think pwede pa iseparate yung mga keywords?
+                }
+
+lines = readFile(filename)
+
+print(lines)
+findLexemes(lines)
+
+print(symbolTable)
