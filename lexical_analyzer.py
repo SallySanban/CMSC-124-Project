9ebfdb807a6fa0e708cba177a8210edf627ef982
sallysanban <johanna.eikou@gmail.com>
2022-11-14 15:38:26 +0800

    test: testing github commit on branch eikou

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,4 +1,17 @@
 import re
 
-def readFile():
-    
+filename = "Project 3/samplecode.txt"
+
+def readFile(filename):
+    file = open(filename)
+    tokens = []
+
+    for line in file.readlines():
+        words = line.split()
+        for i in words:
+            tokens.append(i)
+
+    return tokens
+
+#MAIN CODE
+print(readFile(filename))
