61a5fd1aa00c1c399cb86d28c0c381ccf362e9b7
sallysanban <johanna.eikou@gmail.com>
2022-11-15 15:22:14 +0800

    feat: added functionality to catch literals and identifier (not working)

--- Project 3/lexical_analyzer.py
+++ Project 3/lexical_analyzer.py
@@ -26,18 +26,47 @@ def readFile(filename):
 #finds lexemes in code and groups them 
 def findLexemes(lines):
     for i in lines:
+        identifier = re.findall("\s[a-zA-Z][a-zA-Z0-9_]*\s", i)
+        print(identifier)
+        # if(identifier):
+        #     for j in identifier:
+        #         symbolTable['identifier'].append(j.strip())
+
         #catches NUMBR literal
-        numbrLiteral = re.findall("[-]?\d+", i)
-        if(numbrLiteral):
-            for j in numbrLiteral:
-                symbolTable['NUMBR literal'].append(j)
+        # numbrLiteral = re.findall("[^\.][-]?\d+$", i)
+        # if(numbrLiteral):
+        #     for j in numbrLiteral:
+        #         symbolTable['NUMBR literal'].append(j.strip())
+
+        # numbarLiteral = re.findall("[-]?\d+[.]\d+$", i)
+        # if(numbarLiteral):
+        #     for j in numbarLiteral:
+        #         symbolTable['NUMBAR literal'].append(j.strip())
+
+        # yarnLiteral = re.findall("\".*\"$", i)
+        # if(yarnLiteral):
+        #     for j in yarnLiteral:
+        #         symbolTable['YARN literal'].append(j.strip())
+        
+        # troofLiteral = re.findall("(WIN)$|(FAIL)$", i)
+        # troofLiteral = [tuple(k for k in j if k)[-1] for j in troofLiteral] #[1]
+        # if(troofLiteral):
+        #     for j in troofLiteral:
+        #         symbolTable['TROOF literal'].append(j.strip())
+        
+        # typeLiteral = re.findall("(TROOF)$|(NOOB)$|(NUMBR)$|(NUMBAR)$|(YARN)$|(TYPE)$", i)
+        # typeLiteral = [tuple(k for k in j if k)[-1] for j in typeLiteral]
+        # if(typeLiteral):
+        #     for j in typeLiteral:
+        #         symbolTable['TYPE literal'].append(j.strip())
 
         #add other cases here
+
+def printSymbolTable(symbolTable):
+    space = 5
     
 #MAIN CODE
-symbolTable = {"function identifier": [],
-                "variable identifier": [],
-                "loop identifier": [],
+symbolTable = {"identifier": [],
                 "NUMBR literal": [],
                 "NUMBAR literal": [],
                 "YARN literal": [],
@@ -48,7 +77,11 @@ symbolTable = {"function identifier": [],
 
 lines = readFile(filename)
 
-print(lines)
+#print(lines)
 findLexemes(lines)
 
-print(symbolTable)
+for i in symbolTable:
+    print(i + " = " + str(symbolTable[i]))
+
+#REFERENCES
+# [1] https://stackoverflow.com/questions/24593824/why-does-re-findall-return-a-list-of-tuples-when-my-pattern-only-contains-one-gr
