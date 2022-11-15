d2856ce2f06db31e06d7b99b8e88579f9f59177c
rcducusinjr <rcducusin4@up.edu.ph>
2022-11-15 15:18:54 +0800

    Feat: R -> SMALLR OF Keywords

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -80,7 +80,61 @@ def findLexemes(lines):
             else:
                 symbolTable['keyword'][itzKeyword[0]] = [1]
         
+        rKeyword = re.findall("( R )", i)                          # R
+        if (len(rKeyword) != 0):
+            if (symbolTable['keyword'].get(rKeyword[0].strip())):           # Used the strip to remove leading and trailing whitespaces         (To catch only the letter)
+                symbolTable['keyword']["R"][0] += len(rKeyword)
+            else:
+                symbolTable['keyword'][rKeyword[0].strip()] = [1]
 
+        sumOfKeyword = re.findall("^(SUM\ OF)", i)                          # SUM OF
+        if (len(sumOfKeyword) != 0):
+            if (symbolTable['keyword'].get(sumOfKeyword[0])):           
+                symbolTable['keyword']["SUM OF"][0] += len(sumOfKeyword)
+            else:
+                symbolTable['keyword'][sumOfKeyword[0]] = [1]
+
+        diffOfKeyword = re.findall("^(DIFF\ OF)", i)                          # SUM OF
+        if (len(diffOfKeyword) != 0):
+            if (symbolTable['keyword'].get(diffOfKeyword[0])):           
+                symbolTable['keyword']["SUM OF"][0] += len(diffOfKeyword)
+            else:
+                symbolTable['keyword'][diffOfKeyword[0]] = [1]
+        
+        produktOfKeyword = re.findall("^(PRODUKT\ OF)", i)                          # PRODUKT OF
+        if (len(produktOfKeyword) != 0):
+            if (symbolTable['keyword'].get(produktOfKeyword[0])):           
+                symbolTable['keyword']["SUM OF"][0] += len(produktOfKeyword)
+            else:
+                symbolTable['keyword'][produktOfKeyword[0]] = [1]
+        
+        quoshuntOfKeyword = re.findall("^(QUOSHUNT\ OF)", i)                          # QUOSHUNT OF
+        if (len(quoshuntOfKeyword) != 0):
+            if (symbolTable['keyword'].get(quoshuntOfKeyword[0])):           
+                symbolTable['keyword']["SUM OF"][0] += len(quoshuntOfKeyword)
+            else:
+                symbolTable['keyword'][quoshuntOfKeyword[0]] = [1]
+
+        modOfKeyword = re.findall("^(MOD\ OF)", i)                          # MOD OF
+        if (len(modOfKeyword) != 0):
+            if (symbolTable['keyword'].get(modOfKeyword[0])):           
+                symbolTable['keyword']["SUM OF"][0] += len(modOfKeyword)
+            else:
+                symbolTable['keyword'][modOfKeyword[0]] = [1]
+        
+        biggrOfKeyword = re.findall("^(BIGGR\ OF)", i)                          # BIGGR OF
+        if (len(biggrOfKeyword) != 0):
+            if (symbolTable['keyword'].get(biggrOfKeyword[0])):           
+                symbolTable['keyword']["SUM OF"][0] += len(biggrOfKeyword)
+            else:
+                symbolTable['keyword'][biggrOfKeyword[0]] = [1]
+        
+        smallrOfKeyword = re.findall("^(SMALLR\ OF)", i)                          # SMALLR OF
+        if (len(smallrOfKeyword) != 0):
+            if (symbolTable['keyword'].get(smallrOfKeyword[0])):           
+                symbolTable['keyword']["SUM OF"][0] += len(smallrOfKeyword)
+            else:
+                symbolTable['keyword'][smallrOfKeyword[0]] = [1]
         #add other cases here
     
 #MAIN CODE
